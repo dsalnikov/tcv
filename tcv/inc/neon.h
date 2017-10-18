dac6ca144b5e1fe88531a67f5cee32197b1e21ee
lamazavr <mr.dimas@meta.ua>
2017-10-18 18:19:52 +0300

    neon to sse header added

--- tcv/inc/neon.h
+++ tcv/inc/neon.h
@@ -4,20 +4,7 @@
 #ifdef __ARM_NEON
 	#include "arm_neon.h"
 #else
-
-typedef unsigned char uint8_t;
-
-typedef struct {
-	uint8_t m[16];
-} uint8x16_t;
-
-
-uint8x16_t vld1q_u8(uint8_t const * ptr);
-void vst1q_u8(uint8_t * ptr, uint8x16_t val);
-
-uint8x16_t vminq_u8(uint8x16_t a, uint8x16_t b);
-uint8x16_t vmaxq_u8(uint8x16_t a, uint8x16_t b);
-
+    #include "NEON_2_SSE.h"
 #endif // __ARM_NEON
 
 #endif // __NEON_H__
